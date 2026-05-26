51ff6f2036f3f4396b86c3ac41198aed1bc78ee0
baioka <bajarystanhb@gmail.com>
2026-05-26 22:09:15 +0500

    Update Problem5.java

--- src/Problem5.java
+++ src/Problem5.java
@@ -1,12 +1,33 @@
 public class Problem5 {
-    public int search(int[] A, int key) {
-        // write your code here
+    public static int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] == target) {
+                return mid;
+            }
+            
+            if (nums[left] <= nums[mid]) {
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
         return -1;
-    }
-
-    static void main(String[] args) {
-        // test your code here
-        Problem5 p = new Problem5();
-        System.out.println(p.search(new int[]{4,5,6,7,8,9,10,1,2,3}, 9)); // right answer: 5
     }
 }
