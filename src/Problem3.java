b95933f3f2fb2de5dbccb73f1810fad2357ed7c3
baioka <bajarystanhb@gmail.com>
2026-05-26 21:59:23 +0500

    Update Problem3.java

--- src/Problem3.java
+++ src/Problem3.java
@@ -1,11 +1,24 @@
 public class Problem3 {
-    public int sqrt(int x) {
-        // write your code here
-        return 0;
-    }
-    static void main(String[] args) {
-        // test your code here
-        Problem3 p = new Problem3();
-        System.out.println(p.sqrt(10));
+    public static int mySqrt(int x) {
+        if (x == 0 || x == 1) {
+            return x;
+        }
+        
+        int left = 1;
+        int right = x;
+        int ans = 0;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            // Запись "mid <= x / mid" вместо "mid * mid <= x" предотвращает переполнение типа int
+            if (mid <= x / mid) {
+                ans = mid; // Запоминаем возможный ответ
+                left = mid + 1; // Пытаемся найти большее число
+            } else {
+                right = mid - 1; // Число слишком большое
+            }
+        }
+        return ans;
     }
 }
